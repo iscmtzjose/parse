5460eb35e6b096042b803b3815f045d2bdaf2118
Intesis <izcmtzjose@gmail.com>
2016-03-30 16:19:57 -0600

    cambios

--- index.js
+++ index.js
@@ -2,38 +2,33 @@
 // compatible API routes.
 
 var express = require('express');
-var cors = require('cors'); // add this line below it
+var cors = require('cors') //** add this line
 var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '',
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337',
-  javascriptKey: process.env.JAVASCRIPT_KEY || '',  //** add this line no need to set values, they will be overwritten by heroku config vars
-  restAPIKey: process.env.REST_API_KEY || '', //** add this line
-  dotNetKey: process.env.DOT_NET_KEY || '', //** add this line
-  clientKey: process.env.CLIENT_KEY || '', //** add this line liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+appId: process.env.APP_ID || 'myAppId',
+masterKey: process.env.MASTER_KEY || '',
+javascriptKey: process.env.JAVASCRIPT_KEY || '', //** add this line no need to set values, they will be overwritten by heroku config vars
+restAPIKey: process.env.REST_API_KEY || '', //** add this line
+dotNetKey: process.env.DOT_NET_KEY || '', //** add this line
+clientKey: process.env.CLIENT_KEY || '', //** add this line
+ serverURL: process.env.SERVER_URL || 'localhost:1337' 
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
-var app = express();
-app.use(cors()); // add this line below it
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+var app = express();
+app.use(cors()); //** add this line
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
@@ -41,21 +36,10 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+res.status(200).send('I dream of being a web site.');
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+app.listen(port, function() {
+console.log('parse-server-example running on port ' + port + '.');
 });
-
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
-
